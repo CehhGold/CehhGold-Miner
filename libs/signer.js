e369abac4a572ed05cd8a0e3ae8a682231e29f2e
OFRBG <fi@SaturnMx.local>
2018-07-14 11:19:35 -0500

    0x

--- libs/signer.js
+++ libs/signer.js
@@ -2,7 +2,7 @@ const Web3 = require('web3');
 
 var signWithKey = function(privateKey){
   const web3 = new Web3(new Web3.providers.HttpProvider('https://kovan.infura.io/metamask'));
-  return web3.eth.accounts.sign(web3.utils.soliditySha3({t:'bytes20',v:'0xF7Dc813B5c746F777DD29c94b7558ADE7577064e'}),privateKey);
+  return web3.eth.accounts.sign(web3.utils.soliditySha3({t:'bytes20',v:'0xF7Dc813B5c746F777DD29c94b7558ADE7577064e'}),"0x"+privateKey);
 }
 
 module.exports = {
