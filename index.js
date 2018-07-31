1416b8fb7d1e80ed437d85706f4dc3652e898af1
OFRBG <fi@SaturnMx.local>
2018-07-30 23:49:05 -0500

    default command

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 #! /usr/bin/env node
 
 var chalk = require('chalk');
+const title = chalk.underline(chalk.bgBlack.white("CEHH+ Miner"));
+
+console.clear();
 var argv = require('yargs')
-  .describe('CEHH+ Miner')
   .command(require('./miner'))
   .command(require('./libs/inspect'))
-  .epilog('copyright 2018')
-  .demandCommand(1, 'You need to provide a command. Use ' + chalk.yellow('cehhgold-miner run -a YOUR_ADDRESS') + ' for the miner or ' + chalk.yellow('cehhgold-miner inspect') + ' to open the log reader.')
-  .help('help')
+  .demandCommand(1, title + '\nYou need to provide a command!\n\n' + chalk.yellow('cehhgold-miner run -a YOUR_ADDRESS') + '\n\tfor the miner or \n' + chalk.yellow('cehhgold-miner inspect') + '\n\tto open the log reader.\n\n')
   .argv;
